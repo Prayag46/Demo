a26c7cfa61b11cb8ccd7bb01db0a8ffc83680162
Prayag <Prayag@DESKTOP-Q0P323B>
2020-10-04 18:25:01 +0530

    infogenie

---
+++ infogenie/js/main.js
@@ -0,0 +1,75 @@
+                    var app = angular.module('infogenie', ['angularUtils.directives.dirPagination']);
+                    app.controller('myCtrl', function ($scope) {
+                        $scope.SuppressionList = [
+                            {
+                                'no': 1,
+                                'name': 'Already Contacted',
+                                'modifiedon': '28-Apr-2020-12.39PM',
+                                'modifiedby': 'Chris Jericho',
+                                'records': '0'
+                            },
+                            {
+                                'no': 2,
+                                'name': 'Marketo Leads',
+                                'modifiedon': '25-Feb-2020-02.20PM',
+                                'modifiedby': 'Mark Henry',
+                                'records': '7,800'
+                            },
+                            {
+                                'no': 3,
+                                'name': 'Do not disturb',
+                                'modifiedon': '18-Apr-2020-04.30PM',
+                                'modifiedby': 'John Nick',
+                                'records': '3,500'
+                            },
+                            {
+                                'no': 4,
+                                'name': 'Already Contacted',
+                                'modifiedon': '11-Apr-2020-04.34PM',
+                                'modifiedby': 'Dwen Samuel',
+                                'records': '3,200'
+                            },
+                            {
+                                'no': 5,
+                                'name': 'Already Contacted',
+                                'modifiedon': '28-Apr-2020-12.39PM',
+                                'modifiedby': 'Goldberg',
+                                'records': '6.100'
+                            },
+                            {
+                                'no': 6,
+                                'name': 'Already Contacted',
+                                'modifiedon': '28-Apr-2020-12.39PM',
+                                'modifiedby': 'Mark Henry',
+                                'records': '9.200'
+                            },
+                            {
+                                'no': 7,
+                                'name': 'Already Contacted',
+                                'modifiedon': '28-Apr-2020-12.39PM',
+                                'modifiedby': 'Chris Jericho',
+                                'records': 0
+                            },
+                            {
+                                'no': 8,
+                                'name': 'Already Contacted',
+                                'modifiedon': '25-Apr-2020-12.39PM',
+                                'modifiedby': 'Jeff Hardy',
+                                'records': '3,200'
+                            },
+                            {
+                                'no': 9,
+                                'name': 'Already Contacted',
+                                'modifiedon': '28-Apr-2020-02.39PM',
+                                'modifiedby': 'Chris Jericho',
+                                'records': 0
+                            },
+                            {
+                                'no': 10,
+                                'name': 'Already Contacted',
+                                'modifiedon': '08-Apr-2020-12.19PM',
+                                'modifiedby': 'Mark Henry',
+                                'records': '100'
+                            }
+                        ];
+                    });
